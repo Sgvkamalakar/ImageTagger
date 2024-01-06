7180dbf6c501a7ea219d19aa4ce54236c11e5360
Kamalakar Satapathi <103712713+Sgvkamalakar@users.noreply.github.com>
2024-01-06 11:54:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from PIL import Image
 import google.generativeai as genai
 
 st.title('Image Captioning and Tagging')
+
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 API_KEY = st.text_input("Enter your API Key", type="password")
 if uploaded_file is not None:
@@ -27,3 +28,47 @@ if uploaded_file is not None:
             tags=model.generate_content(["Generate 5 hash tags for the image in a line in english",img])
             st.image(img, caption=f"Caption: {caption.text}")
             st.write(f"Tags: {tags.text}")
+import streamlit as st
+
+footer="""
+  <style>
+        /* Links */
+        a:link, a:visited {
+            color: blue;
+            text-decoration: none; /* Remove underline */
+        }
+
+        a:hover, a:active {
+            color: red;
+            text-decoration: underline; /* Add underline on hover */
+        }
+        .footer .p{
+            font-size:10px;
+        }
+
+        /* Footer */
+        .footer {
+            position:fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            height:10%;
+            font-size:15px;
+            color: white; 
+            text-align: center;
+            padding: 10px 0; 
+        }
+        .footer p{
+            font-size:20px;
+        }
+
+        .footer a:hover {
+            color: white;
+        }
+    </style>
+
+    <div class="footer">
+        <p>Developed with ❤ by <a href="https://www.linkedin.com/in/sgvkamalakar" target="_blank">sgvkamalakar</a></p>
+    </div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
